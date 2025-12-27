afe6af4bf0405063633f2744090d3a14122d955c
ParkEunJe <prk4659@naver.com>
2025-12-28 00:28:58 +0900

    feat: database.py 추가

---
+++ core/database.py
@@ -0,0 +1,68 @@
+import os
+import psycopg2
+from psycopg2.extras import RealDictCursor
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class PostgresDB:
+    """
+    PostgreSQL 데이터베이스 연결을 관리하는 클래스 (Context Manager 지원)
+    사용법:
+        with PostgresDB() as db:
+            db.execute("SELECT * FROM ...")
+    """
+    def __init__(self):
+        self.conn = None
+        self.cursor = None
+
+    def __enter__(self):
+        """with 문에 진입할 때 실행 (연결)"""
+        try:
+            self.conn = psycopg2.connect(
+                host = "localhost",
+                database=os.getenv("POSTGRES_DB"),
+                user=os.getenv("POSTGRES_USER"),
+                password=os.getenv("POSTGRES_PASSWORD"),
+                port=os.getenv("POSTGRES_PORT")
+            )
+
+            # 결과를 딕셔너리 형태 {'name' : '캐릭터명', 'lv' : 1680} 형식으로 받아준다.
+            self.cursor = self.conn.cursor(cursor_factory=RealDictCursor)
+            return self.cursor
+        except Exception as e:
+            print(f"DB 연결 실패: {e}")
+            raise e
+        
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        """with 문을 빠져나갈 때  실행 (자원 해제)"""
+        if self.cursor:
+            self.cursor.close()
+
+        if self.conn:
+            if exc_type:
+                self.conn.rollback() # 에러시 롤백
+            else:
+                self.conn.commit()   # 정상종료시 커밋
+            self.conn.close()
+
+def init_db():
+    """ 필요한 테이블 생성 """
+    create_character_table_sql="""
+    CREATE TABLE IF NOT EXISTS characters (
+        character_name VARCHAR(50) PRIMARY KEY,
+        server_name VARCHAR(20),
+        character_class VARCHAR(20),
+        item_avg_level FLOAT,
+        combat_Power FLOAT,
+        update_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+        )
+    """
+
+    print (" DB 데이터 초기화 중...")
+    try:
+        with PostgresDB() as cur:
+            cur.execute(create_character_table_sql)
+            print("'characters' 테이블 준비 완료")
+    except Exception as e:
+        print(f"테이블 생성 중 오류 발생: {e}")
--- main.py
+++ main.py
@@ -1,6 +1,33 @@
 import streamlit as st
 import sys
 
+from core.database import init_db, PostgresDB
+
+st.set_page_config(page_title="LOA AGENT v2", page_icon="🛡️")
+
 st.title("🛡️ LOA AGENT v2: Architecture Rebuild")
+
+if st.sidebar.button(" DB 초기화 (테이블 생성)"):
+    with st.spinner("테이블 생성중 ..."):
+        init_db()
+        st.success ("초기화 완료")
+
+st.subheader("저장된 캐릭터 목록(DB 조회)")
+
+try:
+    with PostgresDB() as cur:
+        cur.execute("SELECT * FROM characters")
+        rows = cur.fetchall()
+
+        if rows:
+            st.write(rows)
+        else:
+            st.info("noData yes Table")
+
+except Exception as e:
+    st.error(f"DB 연결 오류 {e}")
+
+
+
 st.write(f"Python Version: {sys.version}")
 st.write("Development Environment: Setup Complete!")
